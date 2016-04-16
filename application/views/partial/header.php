67cc1c6c2eab46845ba32f3c7196a01fb772052c
jekkos <jeroen.peelaerts@gmail.com>
2016-04-17 00:20:04 +0200

    Enable bootstrap-tables for item kits module (#293)

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -65,7 +65,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/opensourcepos_bower.css"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=7db4db02d9" language="javascript"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=6e249736ef" language="javascript"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
--- templates/spacelab/views/partial/header.php
+++ templates/spacelab/views/partial/header.php
@@ -68,7 +68,7 @@
 		<!-- end mincss template tags -->
 		<link rel="stylesheet" type="text/css" href="templates/spacelab/css/style.css"/>
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=7db4db02d9" language="javascript"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=6e249736ef" language="javascript"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
